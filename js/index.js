fe20170a182fef1c18ec3310894aa733f774bbb1
rootnure <nur.diu.2791@gmail.com>
2023-08-13 14:41:23 +0600

    HTML Dynamic manipulation [style, getAttribute(), setAttribute(), removeAttribute(), classList, classList.add(), classList.remove(), innerText, innerHTML]

--- js/index.js
+++ js/index.js
@@ -1,14 +1,53 @@
-console.log('hello from js');
+// console.log('hello from js');
 // console.log(document.body);
 
+/* HTML Manipulation */
+
+const fruitsSection0 = document.getElementsByClassName('fruits-container')[0];
+
+// style
+fruitsSection0.style.backgroundColor = 'aqua';
+
+// getAttribute
+const styleAttributeValue = fruitsSection0.getAttribute('style')
+// console.log(styleAttributeValue);
+
+// setAttribute
+fruitsSection0.setAttribute('class', 'bold')
+// console.log(title);
+
+// removeAttribute
+fruitsSection0.removeAttribute('style');
+// console.log(title);
+
+// classList
+const titleClassList = fruitsSection0.classList;
+// console.log(titleClassList);
+fruitsSection0.classList.remove('bold'); // remove an existing class from HTML element
+fruitsSection0.classList.add('bolder'); // add a new class from HTML element
+// console.log(title);
+
+// innerText
+const HTMLContent = fruitsSection0.innerHTML; // return the raw html
+// console.log(HTMLContent);
+const textContent = fruitsSection0.innerText; // return only the visible text in browser
+// console.log(textContent);
+
+
+
+
+// -----------------------------------------------------------------
+
+
+/* Five Ways to get HTML Elements */
 
 // option-5: querySelector
 const singleLi = document.querySelector('.fruits-container li');
-console.log(singleLi);
+// console.log(singleLi);
 
 // option-4: querySelectorAll
 const someLi = document.querySelectorAll('.fruits-container li');
-console.log(someLi);
+// console.log(someLi);
 
 // option-3: getElementsByClassName
 const places = document.getElementsByClassName('visited');
