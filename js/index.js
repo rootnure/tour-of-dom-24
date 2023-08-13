1fa864a82b0e0753513dfde8a987842cf0fd0c91
rootnure <nur.diu.2791@gmail.com>
2023-08-13 14:45:37 +0600

    HTML Dynamic manipulation [style, getAttribute(), setAttribute(), removeAttribute(), classList, classList.add(), classList.remove(), innerText, innerHTML]

--- js/index.js
+++ js/index.js
@@ -3,34 +3,34 @@
 
 /* HTML Manipulation */
 
-const fruitsSection0 = document.getElementsByClassName('fruits-container')[0];
+const element = document.getElementsByClassName('fruits-container')[0];
 
 // style
-fruitsSection0.style.backgroundColor = 'aqua';
+element.style.backgroundColor = 'aqua';
 
 // getAttribute
-const styleAttributeValue = fruitsSection0.getAttribute('style')
+const styleAttributeValue = element.getAttribute('style')
 // console.log(styleAttributeValue);
 
 // setAttribute
-fruitsSection0.setAttribute('class', 'bold')
+element.setAttribute('class', 'bold')
 // console.log(title);
 
 // removeAttribute
-fruitsSection0.removeAttribute('style');
+element.removeAttribute('style');
 // console.log(title);
 
 // classList
-const titleClassList = fruitsSection0.classList;
+const titleClassList = element.classList;
 // console.log(titleClassList);
-fruitsSection0.classList.remove('bold'); // remove an existing class from HTML element
-fruitsSection0.classList.add('bolder'); // add a new class from HTML element
+element.classList.remove('bold'); // remove an existing class from HTML element
+element.classList.add('bolder'); // add a new class from HTML element
 // console.log(title);
 
 // innerText
-const HTMLContent = fruitsSection0.innerHTML; // return the raw html
+const HTMLContent = element.innerHTML; // return the raw html
 // console.log(HTMLContent);
-const textContent = fruitsSection0.innerText; // return only the visible text in browser
+const textContent = element.innerText; // return only the visible text in browser
 // console.log(textContent);
 
 
@@ -63,7 +63,7 @@ fruitsTitle.innerText = 'Fruits Title Changed by JS';
 const liCollections = document.getElementsByTagName('li');
 for (const li of liCollections) {
     // console.log(li.innerText);
-} document.getele
+}
 
 const allHeadings = document.getElementsByTagName('h1');
 for (const h1 of allHeadings) {
