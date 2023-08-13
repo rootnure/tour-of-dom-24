a43b097266042e677789f75d429fcfd33c1036cd
rootnure <nur.diu.2791@gmail.com>
2023-08-13 13:50:29 +0600

    Code moved to external files

--- js/index.js
+++ js/index.js
@@ -1,2 +1,32 @@
 console.log('hello from js');
 // console.log(document.body);
+
+
+// option-5: querySelector
+const singleLi = document.querySelector('.fruits-container li');
+console.log(singleLi);
+
+// option-4: querySelectorAll
+const someLi = document.querySelectorAll('.fruits-container li');
+console.log(someLi);
+
+// option-3: getElementsByClassName
+const places = document.getElementsByClassName('visited');
+for (const place of places) {
+    // console.log(place.innerText)
+}
+
+// option-2: getElementById
+const fruitsTitle = document.getElementById('fruits-title');
+fruitsTitle.innerText = 'Fruits Title Changed by JS';
+
+// option-1: getElementsByTagName
+const liCollections = document.getElementsByTagName('li');
+for (const li of liCollections) {
+    // console.log(li.innerText);
+} document.getele
+
+const allHeadings = document.getElementsByTagName('h1');
+for (const h1 of allHeadings) {
+    // console.log(h1.innerText);
+}
